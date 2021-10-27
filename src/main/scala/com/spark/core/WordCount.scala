12f526388fcfcd1dd6cb922c8180cc30ea7c9fcd
QingQuanFu <fuqingquandl@163.com>
2021-10-27 11:15:54 +0800

    WordCount version1.

--- src/main/scala/com/spark/core/WordCount.scala
+++ src/main/scala/com/spark/core/WordCount.scala
@@ -8,6 +8,14 @@ object WordCount {
     val sparkConfig = new SparkConf().setMaster("local").setAppName("WordCount")
     val sparkContext = new SparkContext(sparkConfig)
 
+    val lines = sparkContext.textFile("src/main/resources/data")
+    lines.flatMap(_.split(" "))
+      .groupBy(x=>x)
+      .map {
+        case (k,v) => (k, v.size)
+      }
+      .collect().foreach(println)
+
     sparkContext.stop()
   }
 
